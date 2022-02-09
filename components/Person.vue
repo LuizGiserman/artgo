5b4d33fedb54ee7b5d303b8d360960da60de870d
LuizGiserman <luizgiserman@poli.ufrj.br>
2022-02-09 18:38:50 +0100

    Initial Commit

---
+++ components/Person.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="img-with-text">
+      <v-img
+        contain
+        :src="src"
+        max-height="35vh"
+        max-width="26.25vw"
+        aspect-ratio="1"
+      ></v-img>
+      <h2> 
+        {{ name }}
+      </h2>
+      <p> 
+        {{ text }}
+      </p>
+    </div>
+</template>
+
+<style>
+.img-with-text {
+  text-align: center;
+  max-width: 26.25vw;
+}
+</style>
+
+<script>
+export default {
+
+    props: 
+    {
+        name: {
+            type: String,
+            default: "Luiz"
+        },
+        src: {
+            type: String,
+            default: "/Luiz.jpeg"
+        },
+        text: {
+            type: String,
+            default: "Luiz est super cool"
+        }  
+    }
+
+
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,133 @@
+<template>
+  <v-app class="custom-background">
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+      height="100vh"
+      color="#ce9c38"
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      color="#5a180c"
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <!-- <v-btn
+        icon
+        @click.stop="miniVariant = !miniVariant"
+      >
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn> -->
+      <!-- <v-btn
+        icon
+        @click.stop="clipped = !clipped"
+      >
+        <v-icon>mdi-application</v-icon>
+      </v-btn> -->
+      <!-- <v-btn
+        icon
+        @click.stop="fixed = !fixed"
+      >
+        <v-icon>mdi-minus</v-icon>
+      </v-btn> -->
+      <router-link to=/>
+        <v-img
+          class="mx-2"
+          contain
+          src=/logo_nom.png
+          max-height="70"
+          max-width="180"
+        ></v-img>
+      </router-link>
+      <v-spacer />
+      <!-- <v-btn
+        icon
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>mdi-menu</v-icon>
+      </v-btn> -->
+    </v-app-bar >
+    <v-main >
+      <v-container >
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <!-- <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer> -->
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: true,
+      drawer: true,
+      fixed: true,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Bienvenue',
+          to: '/'
+        },
+        {
+          icon: 'mdi-calendar',
+          title: 'Agenda',
+          to: '/agenda'
+        },
+        {
+          icon: 'mdi-account-multiple',
+          title: 'Notre equipe',
+          to: '/equipe'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Art'gonautes"
+    }
+  }
+}
+</script>
