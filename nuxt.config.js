3276f5c6c184a2e1e3e2bac226cade09a4c8f46f
LuizGiserman <luizgiserman@poli.ufrj.br>
2022-03-15 08:28:40 +0100

    Removed unwanted plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,9 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -51,11 +53,11 @@ export default {
                code: 'en',
                name: 'English'
             }
-        ],
+          ],
         vueI18n: i18n
       }
-     ]
-    ],
+    ]
+  ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -84,9 +86,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: [
-      "gsap"
-    ]
   }
 }
 
